0cc5c9f908042d8f7b557c2b367af17c22b799d8
indira <indira.sultanova.2002@mail.ru>
2020-05-18 17:34:33 +0500

    commit zadanie4

---
+++ zadanie4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "i", "T", "t", "Y" };
+            list.Remove("K");
+            list.Remove("i");
+            list.Remove("T");
+            list.Remove("t");
+            list.Remove("Y");
+          
+            for(int i=0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine("\n");
+            ArrayList list2 = new ArrayList() { "K", "T","Y" };
+            foreach (object o2 in list2)
+            {
+                Console.WriteLine(o2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
